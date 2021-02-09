c6e7754232df8a7f21adaad4e6dd5a5c2a0620fb
NullSense <matas234@gmail.com>
2021-02-10 00:57:19 +0100

    Add bastyl mini keymap

--- keyboards/hidtech/bastyl/config.h
+++ keyboards/hidtech/bastyl/config.h
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2020 Quentin LEBASTARD <qlebastard@gmail.com>
  * Copyright 2020 Anthony MARIN <anthony@hidtech.ca>
  *
@@ -29,10 +29,20 @@
 #define DIODE_DIRECTION ROW2COL
 #define MATRIX_ROW_PINS { D7, B5, F7, F6, B6 }
 #define MATRIX_COL_PINS { B4, E6, C6, B1, B3, B2 }
+#define TAPPING_TERM    180
+#define IGNORE_MOD_TAP_INTERRUPT // this makes it possible to do rolling combos (zx) with keys that convert to other keys on hold (z becomes ctrl when you hold it, and when this option isn't enabled, z rapidly followed by x actually sends Ctrl-x. That's bad.)
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define TAPPING_FORCE_HOLD
+// Apply the modifier on keys that are tapped during a short hold of a modtap
+#define PERMISSIVE_HOLD
 #define RGB_DI_PIN D2
-#define RGBLED_NUM 38
-#define RGBLIGHT_SPLIT
+#define RGBLED_NUM 42
+#define RGBLIGHT_SPLIT 21
 #define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_HUE_STEP 10
+#define RGBLIGHT_SAT_STEP 10
+#define RGBLIGHT_VAL_STEP 10
+#define RGBLIGHT_LIMIT_VAL 180
 #define DEBOUNCE 5
 #define SOFT_SERIAL_PIN D0
 #define LOCKING_SUPPORT_ENABLE
--- keyboards/hidtech/bastyl/keymaps/default/keymap.c
+++ keyboards/hidtech/bastyl/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Copyright 2020 Quentin LEBASTARD <qlebastard@gmail.com>
  * Copyright 2020 Anthony MARIN <anthony@hidtech.ca>
  *
@@ -18,44 +18,224 @@
 
 #include QMK_KEYBOARD_H
 
+enum {
+    DOT_TD = 0,
+    ONE_SHOT_CAPS,
+};
+
+enum custom_keycodes {
+    EMAIL = SAFE_RANGE,
+    CAPSWRD,
+};
+
+bool caps_word_on = false;
+
+void caps_word_enable(void) {
+    caps_word_on = true;
+    if (!(host_keyboard_led_state().caps_lock)) {
+        tap_code(KC_CAPS);
+    }
+}
+
+void caps_word_disable(void) {
+    caps_word_on = false;
+    if (host_keyboard_led_state().caps_lock) {
+        tap_code(KC_CAPS);
+    }
+}
+
+void caps_word_process_user(uint16_t keycode, const keyrecord_t *record) {
+    // Update caps word state
+    if (caps_word_on) {
+        // Get the base keycode of a mod or layer tap key
+        switch (keycode) {
+            case QK_MOD_TAP ... QK_MOD_TAP_MAX:
+            case QK_LAYER_TAP ... QK_LAYER_TAP_MAX:
+                // Earlier return if this has not been considered tapped yet
+                if (record->tap.count == 0)
+                    return;
+                keycode = keycode & 0xFF;
+                break;
+            default:
+                break;
+        }
+
+        switch (keycode) {
+            // Keycodes to shift
+            case KC_A ... KC_Z:
+                if (record->event.pressed)
+                    caps_word_enable();
+            // Keycodes to ignore (don't disable caps word), but not to shift
+            case KC_MINS:
+            case KC_BSPC:
+            case KC_UNDS:
+            case CAPSWRD:
+                // If mod chording disable the mods
+                if (record->event.pressed && (get_mods() != MOD_LSFT) && (get_mods() != 0)) {
+                    caps_word_disable();
+                }
+                break;
+            default:
+                if (record->event.pressed)
+                    caps_word_disable();
+                break;
+        }
+    }
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+    caps_word_process_user(keycode, record);
+
+    switch (keycode) {
+        case EMAIL:
+            if (record->event.pressed) {
+                SEND_STRING("matas234@gmail.com");
+            }
+            break;
+    }
+    return true;
+};
+
+
+
+void sentence_end(qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+
+        // Double tapping TD_DOT produces
+        // ". <one-shot-shift>" i.e. dot, space and capitalize next letter.
+        // This helps to quickly end a sentence and begin another one
+        // without having to hit shift.
+        case 2:
+            /* Check that Shift is inactive */
+            if (!(get_mods() & MOD_MASK_SHIFT)) {
+                tap_code(KC_SPC);
+                /* Internal code of OSM(MOD_LSFT) */
+                set_oneshot_mods(MOD_BIT(KC_LSHIFT) | get_oneshot_mods());
+
+            } else {
+                // send ">" (KC_DOT + shift → ">")
+                tap_code(KC_DOT);
+            }
+            break;
+
+        // Since `sentence_end` is called on each tap
+        // and not at the end of the tapping term,
+        // the third tap needs to cancel the effects
+        // of the double tap in order to get the expected
+        // three dots ellipsis.
+        case 3:
+            // remove the added space of the double tap case
+            tap_code(KC_BSPC);
+            // replace the space with a second dot
+            tap_code(KC_DOT);
+            // tap the third dot
+            tap_code(KC_DOT);
+            break;
+
+        // send KC_DOT on every normal tap of TD_DOT
+        default:
+            tap_code(KC_DOT);
+    }
+};
+
+void one_shot_caps(qk_tap_dance_state_t *state, void *user_data) {
+    switch (state->count) {
+        case 2:
+            caps_word_enable();
+            break;
+        default:
+            set_oneshot_mods(MOD_LSFT);
+    }
+};
+
+qk_tap_dance_action_t tap_dance_actions[] = {
+      [DOT_TD] = ACTION_TAP_DANCE_FN_ADVANCED(sentence_end, NULL, NULL),
+      [ONE_SHOT_CAPS] = ACTION_TAP_DANCE_FN(one_shot_caps),
+};
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [0] = LAYOUT(
-    KC_ESC,  KC_1, KC_2, KC_3, KC_4, KC_5,			            KC_6, KC_7, KC_8,    KC_9,   KC_0,     KC_BSPC,
+    _______, _______, _______, _______, _______, _______,      _______, _______, _______,    _______,   _______,     _______,
 //-------------------------------------------------//-----------------------------------------------------------//
-    KC_TAB,  KC_Q, KC_W, KC_E, KC_R, KC_T,			            KC_Y, KC_U, KC_I,    KC_O,   KC_P,     KC_MINS,
+    EMAIL,  KC_Q, KC_W, KC_E, KC_R, KC_T,	                                KC_Y, KC_U, KC_I,    KC_O,   KC_P,     LT(5,RGB_TOG),
 //-------------------------------------------------//-----------------------------------------------------------//
-    KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G,	                KC_H, KC_J, KC_K,    KC_L,   KC_SCLN,  KC_QUOT,
+_______, LALT_T(KC_A), LCTL_T(KC_S), LGUI_T(KC_D), LSFT_T(KC_F), KC_G,      KC_H, LSFT_T(KC_J), LGUI_T(KC_K), LCTL_T(KC_L), KC_SCLN, KC_QUOT,
 //-------------------------------------------------//-----------------------------------------------------------//
-    KC_LCTL, KC_Z, KC_X, KC_C, KC_V, KC_B,                  KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH,  KC_BSLS,
+    _______, KC_Z, KC_X, KC_C, KC_V, KC_B,                                  KC_N, KC_M, KC_COMM, TD(DOT_TD), KC_SLSH,  RGB_VAD,
 //-------------------------------------------------//-----------------------------------------------------------//
-                        KC_LCTL, KC_SPC, MO(1),		     MO(2),  KC_ENT, KC_RGUI,
-                                 KC_HOME,  KC_BSPC,      KC_DEL, KC_RALT
+           LT(4,KC_TAB), LT(1,KC_BSPC), TD(ONE_SHOT_CAPS),                KC_ENT, LT(2,KC_SPC), LT(3,KC_ESC),
+
+                                    _______,  _______,       _______, _______
   ),
 
   [1] = LAYOUT(
-    KC_TILD, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,			  KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN,  KC_DEL,
+    _______, _______, _______, _______, _______, _______,		_______, _______, _______, _______, _______, _______,
 //---------------------------------------------------------//-----------------------------------------------------------//
-    RESET,   _______, _______, _______, _______, KC_LBRC,			  KC_RBRC, KC_P7,   KC_P8,   KC_P9,   _______,  KC_PLUS,
+    _______,   _______, _______, _______, _______, _______,		_______, KC_7,   KC_8,   KC_9,   _______,  _______,
 //---------------------------------------------------------//-----------------------------------------------------------//
-    _______, KC_HOME, KC_PGUP, KC_PGDN, KC_END,  KC_LPRN,	      KC_RPRN, KC_P4,   KC_P5,   KC_P6,   KC_MINS,  KC_PIPE,
+    _______, _______, _______, KC_PPLS, KC_PAST, _______,	    KC_DOT, KC_4,   KC_5,   KC_6,   _______,  _______,
 //---------------------------------------------------------//-----------------------------------------------------------//
-    _______, _______, _______, _______, _______, _______,       _______, KC_P1,   KC_P2,   KC_P3,   KC_EQL,   KC_UNDS,
+    _______, _______, _______, KC_MINS, KC_SLSH, _______,       KC_COMM, KC_1,   KC_2,   KC_3,   _______,   _______,
 //---------------------------------------------------------//-----------------------------------------------------------//
-                               KC_LCTL, KC_HOME, KC_TRNS,		    KC_TRNS, KC_RALT, KC_RGUI,
-                                        KC_SPC,  KC_BSPC,       KC_RCTL, KC_ENT
+                               _______, LT(1,KC_BSPC), _______,		    KC_LSFT, KC_0, KC_EQL,
+
+                                        _______,  _______,       _______, _______
   ),
 
   [2] = LAYOUT(
-    KC_F12,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,			    KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,
+_______, _______, _______, _______, _______, _______,			  _______, _______, _______, _______, _______, _______,
 //---------------------------------------------------------//--------------------------------------------------------------//
-    _______, _______, RGB_RMOD, RGB_TOG, RGB_MOD, KC_LBRC,			  KC_RBRC, _______, KC_NLCK, KC_INS,  KC_SLCK,  KC_MUTE,
+    _______, _______,  KC_PIPE,   KC_ASTR, KC_AMPR, KC_PLUS,		  KC_MINS, KC_LBRC, KC_CIRC, KC_RBRC, _______,  RESET,
 //---------------------------------------------------------//--------------------------------------------------------------//
-    _______, KC_LEFT, KC_UP,    KC_DOWN, KC_RGHT, KC_LPRN,	      KC_RPRN, KC_MPRV, KC_MPLY, KC_MNXT, _______,  KC_VOLU,
+    _______, _______, KC_TILD,  KC_EXLM,  KC_EQL,  KC_AT,   	      KC_UNDS, KC_LPRN, KC_DLR,  KC_RPRN,  _______,  EEP_RST,
 //---------------------------------------------------------//--------------------------------------------------------------//
-    _______, _______, _______,  _______, _______, _______,        _______, _______, _______, _______, _______,  KC_VOLD,
+    _______, _______, KC_LT,    KC_GT,   KC_GRV,  KC_PERC,        KC_BSLS, KC_LCBR, KC_HASH, KC_RCBR, _______,  _______,
 //---------------------------------------------------------//--------------------------------------------------------------//
-                               KC_LCTL, KC_HOME, KC_TRNS,		    KC_TRNS, KC_RALT, RESET,
-                                        KC_SPC,  KC_BSPC,       KC_RCTL, KC_ENT
+                               _______, KC_DEL, _______,		    _______, LT(2,KC_SPC), _______,
+
+                                        _______,  _______,       _______, _______
   ),
+
+  [3] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,			  _______, _______, _______, _______, _______, _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    _______, _______,  _______,   _______, _______, _______,		  KC_HOME, KC_PGDN, KC_PGUP, KC_END, _______,  _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    _______, _______, _______, KC_CUT, KC_COPY, KC_PSTE,     	    KC_LEFT, KC_DOWN, KC_UP, KC_RGHT,  _______,  _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    _______, _______, _______, _______, KC_UNDO, KC_AGIN,        _______, KC_PSCR, LGUI(KC_INS), G(S(KC_INS)),_______,  _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+                               _______, LCTL(KC_BSLS), _______,		    _______, _______, LT(3,KC_ESC),
+
+                                        _______,  _______,       _______, _______
+  ),
+
+  [4] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,			  _______, _______, _______, _______, _______, _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    _______, _______,  _______,   _______, _______, _______,		  _______, _______, _______, _______, _______,  _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    _______, _______, _______,   _______, _______, _______,     	  _______, KC_VOLD, KC_VOLU, _______,  _______,  _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    _______, _______, _______, _______, _______, _______,             _______, KC_MUTE, _______, _______, _______, _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+                               LT(4,KC_TAB), _______,_______,		    KC_MPRV, KC_MPLY, KC_MNXT,
+
+
+                                        _______,  _______,       _______, _______
+  ),
+  [5] = LAYOUT(
+    _______, _______, _______, _______, _______, _______,			  _______, _______, _______, _______, _______, _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    _______, _______, _______, _______, _______, _______,	        _______, RGB_SPD, RGB_SPI, _______, _______,  TO(5),
+//---------------------------------------------------------//--------------------------------------------------------------//
+    _______, _______, _______, _______, _______, _______,     	    RGB_SAI, RGB_VAD, RGB_VAI, _______,  _______,  _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+    _______, _______, _______, _______, _______, _______,             RGB_SAD, RGB_HUD, RGB_HUI, _______, _______, _______,
+//---------------------------------------------------------//--------------------------------------------------------------//
+                               _______,_______, _______,		      RGB_RMOD, RGB_TOG, RGB_MOD,
+
+                                        _______,  _______,       _______, _______
+  )
 };
